62fd312048f7b2e436a5a4236942b9f34dbaa0fb
John Anderson <sontek@gmail.com>
2010-11-09 22:00:03 -0600

    refactored so we can support ncurses later

--- pyquil.py
+++ pyquil.py
@@ -14,30 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-import pygtk
-pygtk.require('2.0')
-import gtk
-import pango
-from editor import PyQuilEditor
-
-class PyQuilWindow(gtk.Window):
-    def __init__(self):
-        super(PyQuilWindow, self).__init__()
-        self.set_title('PyQuil')
-        win = gtk.ScrolledWindow()
-        self.add(win)
-        self.textview = PyQuilEditor()
-        win.add(self.textview)
-        self.connect('delete-event', lambda *a: gtk.main_quit())
-
-        self.textview.modify_font(pango.FontDescription('monospace'))
-
-        self.resize(800, 500)
-        self.show_all()
-
-    def run(self):
-        gtk.main()
+from window import PyQuilWindow
 
 if __name__ == '__main__':
     PyQuilWindow().run()
---
+++ window.py
@@ -0,0 +1,23 @@
+import pygtk
+pygtk.require('2.0')
+import gtk
+import pango
+from editor import PyQuilEditor
+
+class PyQuilWindow(gtk.Window):
+    def __init__(self):
+        super(PyQuilWindow, self).__init__()
+        self.set_title('PyQuil')
+        win = gtk.ScrolledWindow()
+        self.add(win)
+        self.textview = PyQuilEditor()
+        win.add(self.textview)
+        self.connect('delete-event', lambda *a: gtk.main_quit())
+
+        self.textview.modify_font(pango.FontDescription('monospace'))
+
+        self.resize(800, 500)
+        self.show_all()
+
+    def run(self):
+        gtk.main()
