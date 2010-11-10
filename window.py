de0a15fef9a6934a577d799f29e67ffe16e555f5
John Anderson <sontek@gmail.com>
2010-11-09 22:20:12 -0600

    Added a menubar and tabbed interface

--- window.py
+++ window.py
@@ -7,14 +7,58 @@ from editor import PyQuilEditor
 class PyQuilWindow(gtk.Window):
     def __init__(self):
         super(PyQuilWindow, self).__init__()
-        self.set_title('PyQuil')
-        win = gtk.ScrolledWindow()
-        self.add(win)
-        self.textview = PyQuilEditor()
-        win.add(self.textview)
-        self.connect('delete-event', lambda *a: gtk.main_quit())
+
         self.resize(800, 500)
+        self.set_title('PyQuil')
+
+        self.vbox = vbox  = gtk.VBox()
+        self.add(vbox)
+
+        self.menubar = gtk.MenuBar()
+        self.menus = {}
+        self.menubar_items = {}
+
+        vbox.pack_start(self.menubar, False)
+        
+        self.notebook = notebook = gtk.Notebook()
+        notebook.set_tab_pos(gtk.POS_TOP)
+        notebook.set_scrollable(True)
+        notebook.show_all()
+        vbox.pack_start(notebook)
+        
+        self.documents = []
+        
+        self.add_menu('file', '_File', None)
+
+        self.connect('delete-event', lambda *a: gtk.main_quit())
+
+        self.add_fresh_document()
+
         self.show_all()
+
+    def add_fresh_document(self, position=-1):
+        document = gtk.ScrolledWindow()
+        document.add(PyQuilEditor())
+
+        self.notebook.insert_page(document)
+        if position == -1:
+            self.documents.append(document)
+        else:
+            self.documents.insert(position, document)
+        return document
+
+    def add_menu(self, name, title, menu=None, position=None):
+        item = gtk.MenuItem(title)
+        if menu is None:
+            menu = gtk.Menu()
+        item.set_submenu(menu)
+        self.menus[name] = menu
+        self.menubar_items[name] = item
+        if position is None:
+            self.menubar.append(item)
+        else:
+            self.menubar.insert(item, position)
+        return menu
 
     def run(self):
         gtk.main()
