b36b15e93471ce3396f189ffede818f7c53ea60f
John Anderson <sontek@gmail.com>
2010-11-10 18:03:57 -0600

    got querying working

--- window.py
+++ window.py
@@ -4,6 +4,9 @@ import gtk
 import pango
 from editor import PyQuilEditor
 
+from sqlalchemy import create_engine
+import pyodbc
+
 class PyQuilWindow(gtk.Window):
     def __init__(self):
         super(PyQuilWindow, self).__init__()
@@ -19,6 +22,10 @@ class PyQuilWindow(gtk.Window):
         self.menubar_items = {}
 
         vbox.pack_start(self.menubar, False)
+
+        self.connection_string = gtk.Entry()
+        self.connection_string.set_text('sqlite:///test.db')
+        vbox.pack_start(self.connection_string)
         
         self.notebook = notebook = gtk.Notebook()
         notebook.set_tab_pos(gtk.POS_TOP)
@@ -30,9 +37,15 @@ class PyQuilWindow(gtk.Window):
 
         file_menu = gtk.Menu()
         menu = self.add_menu('file', '_File', file_menu)
+
         new = gtk.ImageMenuItem(gtk.STOCK_NEW)
         new.connect('activate', self.new_file)
         menu.append(new)
+
+        execute = gtk.MenuItem('Execute')
+        execute.connect('activate', self.execute)
+        menu.append(execute)
+
         quit = gtk.ImageMenuItem(gtk.STOCK_QUIT)
         quit.connect('activate', self.quit)
         menu.append(quit)
@@ -45,7 +58,9 @@ class PyQuilWindow(gtk.Window):
 
     def add_fresh_document(self, position=-1):
         document = gtk.ScrolledWindow()
-        document.add(PyQuilEditor())
+        self.editor = PyQuilEditor()
+        self.editor.get_buffer().set_text("SELECT * FROM memos")
+        document.add(self.editor)
 
         self.notebook.insert_page(document)
         if position == -1:
@@ -75,6 +90,31 @@ class PyQuilWindow(gtk.Window):
 
     def quit(self, *args):
         gtk.main_quit()
+
+    def execute(self, *args):
+        buf = self.editor.get_buffer()
+        start = buf.get_start_iter()
+        end = buf.get_end_iter()
+        query = buf.get_slice(start, end)
+        conn_string = self.connection_string.get_text() 
+        e = create_engine(conn_string)
+        connection = e.connect()
+        results = connection.execute(query)
+        connection.close()
+        result_view = gtk.ScrolledWindow()
+        result_text = gtk.TextView()
+        data = results.fetchall()
+        text = ''
+
+        for item in data:
+            text +='\n' + str(item)
+
+        import ipdb;ipdb.set_trace()
+        result_text.get_buffer().set_text(text)
+        result_view.add(result_text)
+        self.vbox.pack_start(result_view)
+        result_text.show()
+        result_view.show()
 
     def run(self):
         gtk.main()
