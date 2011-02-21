8b4b22347a2fd85f08254027f69817e2145398ca
John Anderson <sontek@gmail.com>
2011-02-21 15:42:54 -0600

    support for mssql and if no data is found

--- window.py
+++ window.py
@@ -9,6 +9,10 @@ from sqlalchemy import create_engine
 import pyodbc
 
 class PyQuilWindow(gtk.Window):
+    def con(self):
+        """ This method is required so we can get version 8 of TDS """
+        return pyodbc.connect(self.connection_string.get_text().split(':///')[1])
+
     def __init__(self):
         super(PyQuilWindow, self).__init__()
         self.tree_view = None
@@ -101,55 +105,62 @@ class PyQuilWindow(gtk.Window):
         start = buf.get_start_iter()
         end = buf.get_end_iter()
         query = buf.get_slice(start, end)
-        conn_string = self.connection_string.get_text() 
-        e = create_engine(conn_string)
+        conn_string = self.connection_string.get_text()
+        protocol = conn_string.split(':///')[0]
+
+        if protocol.find('mssql') >= 0:
+            e = create_engine(protocol + ':///', creator=self.con)
+        else:
+            e = create_engine(conn_string)
+
         connection = e.connect()
         results = connection.execute(query)
-        connection.close()
         data = results.fetchall()
+        connection.close()
 
         types = []
-        for item in data[0]:
-            if str(item).isdigit():
-                types.append(gobject.TYPE_INT)
-            else:
-                types.append(gobject.TYPE_STRING)
+        if data:
+            for item in data[0]:
+                if str(item).isdigit():
+                    types.append(gobject.TYPE_INT)
+                else:
+                    types.append(gobject.TYPE_STRING)
 
-        liststore = gtk.ListStore(*types)
+            liststore = gtk.ListStore(*types)
 
-        for row in [b for a, b in enumerate(data)]:
-            liststore.append([b for a, b in enumerate(row)])
-        
-        if (self.tree_view):
-            self.result_view.remove(self.tree_view)
+            for row in [b for a, b in enumerate(data)]:
+                liststore.append([b for a, b in enumerate(row)])
 
-        self.tree_view = gtk.TreeView(model=liststore)
+            if (self.tree_view):
+                self.result_view.remove(self.tree_view)
 
-        tvcolumns={} # the columns
-        cells={} # the cells
-        i=0
-        for c in results.keys(): # the actual column headers
-            # instantiate TVC
-            tvcolumns[c] = gtk.TreeViewColumn(c)
+            self.tree_view = gtk.TreeView(model=liststore)
 
-            # add to the treeview
-            self.tree_view.append_column(tvcolumns[c])
+            tvcolumns={} # the columns
+            cells={} # the cells
+            i=0
+            for c in results.keys(): # the actual column headers
+                # instantiate TVC
+                tvcolumns[c] = gtk.TreeViewColumn(c)
 
-            # instantiate and add the cell object
-            cells[c]=gtk.CellRendererText()
-            tvcolumns[c].pack_start(cells[c], True) #add the cell to the column, allow it to expand
+                # add to the treeview
+                self.tree_view.append_column(tvcolumns[c])
 
-            # now set the cell's text attribute to the treeview's column i (0,1)
-            tvcolumns[c].add_attribute(cells[c], 'text', i)
+                # instantiate and add the cell object
+                cells[c]=gtk.CellRendererText()
+                tvcolumns[c].pack_start(cells[c], True) #add the cell to the column, allow it to expand
 
-            #make it searchable and sortable
-            self.tree_view.set_search_column(i)
-            tvcolumns[c].set_sort_column_id(i)
-            i+=1
+                # now set the cell's text attribute to the treeview's column i (0,1)
+                tvcolumns[c].add_attribute(cells[c], 'text', i)
 
-        self.result_view.add(self.tree_view)
-        self.result_view.show()
-        self.tree_view.show()
+                #make it searchable and sortable
+                self.tree_view.set_search_column(i)
+                tvcolumns[c].set_sort_column_id(i)
+                i+=1
+
+            self.result_view.add(self.tree_view)
+            self.result_view.show()
+            self.tree_view.show()
 
     def run(self):
         gtk.main()
