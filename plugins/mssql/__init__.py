b2dfd14a57b08838748b159bfa9de887d850f7f1
John Anderson <sontek@gmail.com>
2011-03-06 03:22:39 -0600

    handle updates/inserts properly

--- plugins/mssql/__init__.py
+++ plugins/mssql/__init__.py
@@ -1,5 +1,8 @@
 from sqlalchemy import create_engine
+from sqlalchemy.exc import ResourceClosedError
 import pyodbc
+
+from lib.common import _
 
 class MSSQLQueryPlugin(object):
     def con(self):
@@ -7,12 +10,20 @@ class MSSQLQueryPlugin(object):
         return pyodbc.connect(self.connection_string)
 
     def execute_query(self, connection_string, query):
-        self.connection_string = connection_string
-        e = create_engine('mssql+pyodbc:///', creator=self.con)
-        connection = e.connect()
-        results = connection.execute(query)
-        data = results.fetchall()
-        e.dispose()
-        connection.close()
+        try:
+            self.connection_string = connection_string
+            e = create_engine('mssql+pyodbc:///', creator=self.con)
+            connection = e.connect()
+            results = connection.execute(query)
+            data = results.fetchall()
+        except ResourceClosedError as exc:
+            if str(exc) == 'This result object does not return rows. It has been closed automatically.':
+                return [(_('Results', ))], [(_('%d Row(s) modified' % results.rowcount), )]
+                print exc
+            else:
+                raise exc
+        finally:
+            e.dispose()
+            connection.close()
 
         return results.keys(), data
